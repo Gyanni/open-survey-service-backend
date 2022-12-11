c2ae31355bf9a72529534c9b9e258966e8cdd466
Gyanni <lghlee36@gmail.com>
2022-12-11 22:41:05 +0900

    Initial commit

---
+++ src/main/java/com/teamhw/opensurveyservice/OpenSurveyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teamhw.opensurveyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSurveyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSurveyServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamhw/opensurveyservice/OpenSurveyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamhw.opensurveyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSurveyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
